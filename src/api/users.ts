921c1fcf9ae1ff4425b2f496a12436bd2032594a
Fabian Kjellberg <Fabian.Kjellberg98@gmail.com>
2026-01-30 14:01:09 +0100

    smol change

--- src/api/users.ts
+++ src/api/users.ts
@@ -1,7 +1,7 @@
 import { apiClient } from './client';
 
 export async function createAccount(username: string, password: string): Promise<void> {
-  const response = await apiClient('POST', '/api/users', { username, password });
+  const response = await apiClient('POST', '/auth/register', { username, password });
 
   console.log('response', response);
 }
